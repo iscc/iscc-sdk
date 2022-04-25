2e1f3888568e93b61bb3294e34a74e7793bea8b2
Titusz Pan <titusz.pan@gmail.com>
2022-04-25 10:57:34 +0200

    update dependencies

--- tests/test_models.py
+++ tests/test_models.py
@@ -28,8 +28,8 @@ def test_IsccMeta_dict_defaults_exclude_none_unset():
 def test_IsccMeta_json():
     im = iss.IsccMeta(iscc="ISCC:MEAJU5AXCPOIOYFL")
     assert im.json() == (
-        '{"@context": "http://purl.org/iscc/context/0.3.6.jsonld", "@type": '
-        '"CreativeWork", "$schema": "http://purl.org/iscc/schema/0.3.6.json", "iscc": '
+        '{"@context": "http://purl.org/iscc/context/0.3.7.jsonld", "@type": '
+        '"CreativeWork", "$schema": "http://purl.org/iscc/schema/0.3.7.json", "iscc": '
         '"ISCC:MEAJU5AXCPOIOYFL"}'
     )
 
@@ -37,8 +37,8 @@ def test_IsccMeta_json():
 def test_IsccMeta_jcs():
     im = iss.IsccMeta(iscc="ISCC:MEAJU5AXCPOIOYFL")
     assert im.jcs() == (
-        b'{"$schema":"http://purl.org/iscc/schema/0.3.6.json","@context":"http://purl.'
-        b'org/iscc/context/0.3.6.jsonld","@type":"CreativeWork","iscc":"ISCC:MEAJU5AXC'
+        b'{"$schema":"http://purl.org/iscc/schema/0.3.7.json","@context":"http://purl.'
+        b'org/iscc/context/0.3.7.jsonld","@type":"CreativeWork","iscc":"ISCC:MEAJU5AXC'
         b'POIOYFL"}'
     )
 
