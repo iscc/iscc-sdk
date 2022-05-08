a37639d58b308c2349d6ce9f662172e02a686cc6
Titusz Pan <titusz.pan@gmail.com>
2022-05-08 18:20:44 +0200

    use parallel installer

--- iscc_sdk/install.py
+++ iscc_sdk/install.py
@@ -6,26 +6,13 @@ from . import tools
 
 def main():
     log.add(sys.stdout)
-
-    tools.ipfs_install()
+    tools.install()
     log.info(f"IPFS Version: {tools.ipfs_version_info()}")
-
-    tools.exiv2_install()
     log.info(f"Exiv2 Version: {tools.exiv2_version_info()}")
-
-    tools.fpcalc_install()
     log.info(f"FPCALC Version: {tools.fpcalc_version_info()}")
-
-    tools.ffprobe_install()
     log.info(f"FFPROBE Version: {tools.ffprobe_version_info()}")
-
-    tools.ffmpeg_install()
     log.info(f"FFMPEG Version: {tools.ffmpeg_version_info()}")
-
-    tools.java_install()
     log.info(f"JAVA Version: {tools.java_version_info()}")
-
-    tools.tika_install()
     log.info(f"TIKA Version: {tools.tika_version_info()}")
 
     sys.exit(0)
