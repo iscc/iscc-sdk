e96c3ff73c1f6c8a30078bd4ef55ab40379d9081
Titusz Pan <titusz.pan@gmail.com>
2022-05-08 18:43:06 +0200

    log install folder and cleanup imports

--- iscc_sdk/install.py
+++ iscc_sdk/install.py
@@ -1,18 +1,19 @@
 """Installer script for binary dependencies"""
 import sys
 from loguru import logger as log
-from . import tools
+import iscc_sdk as idk
 
 
 def main():
     log.add(sys.stdout)
-    tools.install()
-    log.info(f"IPFS Version: {tools.ipfs_version_info()}")
-    log.info(f"Exiv2 Version: {tools.exiv2_version_info()}")
-    log.info(f"FPCALC Version: {tools.fpcalc_version_info()}")
-    log.info(f"FFMPEG Version: {tools.ffmpeg_version_info()}")
-    log.info(f"JAVA Version: {tools.java_version_info()}")
-    log.info(f"TIKA Version: {tools.tika_version_info()}")
+    log.info(f"Installing tools to {idk.dirs.user_data_dir}")
+    idk.tools.install()
+    log.info(f"IPFS Version: {idk.tools.ipfs_version_info()}")
+    log.info(f"Exiv2 Version: {idk.tools.exiv2_version_info()}")
+    log.info(f"FPCALC Version: {idk.tools.fpcalc_version_info()}")
+    log.info(f"FFMPEG Version: {idk.tools.ffmpeg_version_info()}")
+    log.info(f"JAVA Version: {idk.tools.java_version_info()}")
+    log.info(f"TIKA Version: {idk.tools.tika_version_info()}")
 
     sys.exit(0)
 
