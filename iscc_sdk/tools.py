3039edc94157e0f2137d0a9fea64d68998d27be5
Titusz Pan <titusz.pan@gmail.com>
2022-11-24 18:49:41 +0100

    Fix formatting

--- iscc_sdk/tools.py
+++ iscc_sdk/tools.py
@@ -128,25 +128,25 @@ def extract(archive):  # pragma: no cover
 
     elif archive.endswith("tar.gz"):
         with tarfile.open(archive, "r:gz") as tar_file:
+
             def is_within_directory(directory, target):
-                
+
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-            
+
                 prefix = os.path.commonprefix([abs_directory, abs_target])
-                
+
                 return prefix == abs_directory
-            
+
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-            
+
                 for member in tar.getmembers():
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
-            
-                tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
-            
+
+                tar.extractall(path, members, numeric_owner=numeric_owner)
+
             safe_extract(tar_file, Path(archive).parent.absolute())
     os.unlink(archive)
 
