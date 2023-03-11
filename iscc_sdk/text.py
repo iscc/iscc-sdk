31c29e4078bda60ad099186b5f6f1df77dc5fbd1
Titusz Pan <titusz.pan@gmail.com>
2023-03-11 14:01:34 +0100

    Add custom handling of docx metadata embedding

--- iscc_sdk/text.py
+++ iscc_sdk/text.py
@@ -90,6 +90,8 @@ def text_meta_embed(fp, meta):
         return idk.pdf_meta_embed(fp, meta)
     if mt == "application/epub+zip":
         return idk.epub_meta_embed(fp, meta)
+    if mt == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        return idk.docx_meta_embed(fp, meta)
 
 
 def text_extract(fp):
