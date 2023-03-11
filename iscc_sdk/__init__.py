24730fdc766b843c9abaf453f2eb66e51f63049c
Titusz Pan <titusz.pan@gmail.com>
2023-03-11 13:59:29 +0100

    Add docx support

--- iscc_sdk/__init__.py
+++ iscc_sdk/__init__.py
@@ -27,4 +27,5 @@ from iscc_sdk.exceptions import *
 from iscc_sdk.metadata import *
 from iscc_sdk.pdf import *
 from iscc_sdk.epub import *
+from iscc_sdk.docx_ import *
 from iscc_sdk.thumbnail import *
