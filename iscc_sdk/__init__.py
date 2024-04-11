8e23b7d890df799dce3d171abb3e7edebc3ee7b7
Titusz Pan <titusz.pan@gmail.com>
2024-04-11 20:43:02 +0200

    Prepare v0.6.2 development iteration.

--- iscc_sdk/__init__.py
+++ iscc_sdk/__init__.py
@@ -2,7 +2,7 @@
 import os
 from platformdirs import PlatformDirs
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 APP_NAME = "iscc-sdk"
 APP_AUTHOR = "iscc"
 dirs = PlatformDirs(appname=APP_NAME, appauthor=APP_AUTHOR)
